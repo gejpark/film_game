05697158b01de4edf5d8bd2b28a987210aa5a299
gejpark <122942803+gejpark@users.noreply.github.com>
2023-06-01 20:09:05 -0700

    separate UI prefab created

---
+++ src/prefabs/ui.js
@@ -0,0 +1,109 @@
+class UI extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        // scene.physics.add.existing(this);
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        this.anims.create({
+            key: 'cursor_animation',
+            frames: this.anims.generateFrameNumbers('cursor', {start: 0, end: 3, first: 0}),
+            frameRate: 15,
+            repeat: -1,
+        })
+        this.anims.play('cursor_animation');
+        this.scene.input.mouse.disableContextMenu(); //disable context menu popup on left click
+        this.showCursor = true;
+
+        //controls
+        KEY_P = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
+        MOUSE_POINTER = this.scene.input.activePointer;
+
+        //create revolver UI
+        this.shot_count = 7;
+        this.can_fire = true;
+        this.can_reload = true;
+        this.current_rotation = 0;
+        this.final_rotation = 0;
+        this.rotation_multiplier = 1;
+        this.rotation_lerp = 0;
+
+        //rect for detection
+        this.rect = this.scene.add.rectangle(this.x, this.y, this.width, this.height).setStrokeStyle(2, 0xffff00); //.setStrokeStyle(2, 0xffff00)
+
+        //revolver sprite
+        this.scene.revolver_ui = this.scene.add.sprite(32, game.config.height-32, 'revolver_ui_base_image');
+    }
+
+    update() {
+        //rect for detection
+        this.rect.x = this.x; //set rect
+        this.rect.y = this.y; 
+        const { left, top, width, height } = this.rect.getBounds();
+        const bodiesInRect = this.scene.physics.overlapRect(left, top, width, height); //detect overlapping bodies
+        // console.log(bodiesInRect);
+
+
+        if(this.rotation_lerp < 1.0) {
+            this.scene.revolver_ui.angle = Phaser.Math.Linear(this.current_rotation, this.final_rotation, this.rotation_lerp);
+            this.rotation_lerp += 0.1;
+        }
+        this.scene.revolver_ui.setTexture(`revolver_ui${this.shot_count}`);
+        
+        if (MOUSE_POINTER.leftButtonDown() && this.can_fire == true) {   //left key down
+            this.setScale(0.75);
+            if (this.shot_count > 1) {
+                this.shot_count -= 1;
+                this.final_rotation += 60;
+                this.rotation_lerp = 0;
+            }
+            this.can_fire = false;
+        } 
+        
+        if (!MOUSE_POINTER.leftButtonDown()) {                                //left key up
+            this.setScale(1.0);
+            this.can_fire = true;
+        }
+
+        if (MOUSE_POINTER.rightButtonDown() && this.can_reload == true) {
+            if (this.shot_count < 7) {
+                this.shot_count += 1;
+                this.final_rotation -= 60;
+                this.rotation_lerp = 0;
+            }
+            this.can_reload = false;
+        } 
+        
+        if (!MOUSE_POINTER.rightButtonDown()) {
+            this.can_reload = true;
+        }
+
+        if (Phaser.Input.Keyboard.JustDown(KEY_P)) { //turn cursor on or off (visually)
+            if(this.showCursor == true) {
+                this.showCursor = false;
+            } else {
+                this.showCursor = true;
+            }
+        }
+        if(this.showCursor == true) { //turn cursor on or off (visually)
+            document.body.style.cursor = '';
+        } else {
+            document.body.style.cursor = 'none';
+        }
+        // // console.log(this.showCursor);
+
+        this.x = MOUSE_POINTER.x;
+        this.y = MOUSE_POINTER.y;
+    }
+
+    onOverlap(other) {
+        if (other.active) {
+            other.destroy();
+        }
+    }
+}
--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -2,10 +2,7 @@ class menu extends Phaser.Scene {
     constructor() {
         super('menu');
     }
-    preload() {
-        this.load.spritesheet('cursor','./assets/cursor.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 3});
-        this.load.image('cursor_base', './assets/cursor1.png');
-        
+    preload() {    
         //trailer background image
         this.trailer_images = [];
         this.trailer_base_image = this.load.image('trailer_base_image', './assets/trailer_images/1.png');
@@ -21,6 +18,9 @@ class menu extends Phaser.Scene {
             this.load.image(`revolver_ui${i}`, `./assets/revolver_ui/${i}.png`);
             this.revolver_ui_frames.push({key: `revolver_ui${i}`});
         }
+        //cursor UI
+        this.load.spritesheet('cursor','./assets/cursor.png', {frameWidth: 32, frameHeight: 32, startFrame: 0, endFrame: 3});
+        this.load.image('cursor_base', './assets/cursor1.png');
     }
     create() {        
 
@@ -35,22 +35,20 @@ class menu extends Phaser.Scene {
         this.menu_background = this.add.sprite(640/2,480/2,'trailer_base_image').setOrigin(0.5);
         this.menu_background.anims.play('trailer', true);
         this.cameras.main.setBackgroundColor(0xbababa); //set background color for main menu
-        this.input.mouse.disableContextMenu(); //disable context menu popup on left click
+        // this.input.mouse.disableContextMenu(); //disable context menu popup on left click
         
-        this.rect = this.add.rectangle(400, 300, 32, 32); //.setStrokeStyle(2, 0xffff00)
-        // this.image1 = this.add.image(320, 320, 'cursor_base'); // default origin is 0.5 = the center
-        this.cursor = this.add.sprite(0, 0, 'cursor_base');
-        this.anims.create({
-            key: 'cursor_animation',
-            frames: this.anims.generateFrameNumbers('cursor', {start: 0, end: 3, first: 0}),
-            frameRate: 15,
-            repeat: -1,
-        })
-        this.cursor.anims.play('cursor_animation');
-        // KEY_ESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESCAPE);
-        // KEY_SPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.UI = new UI(this,300,300, 'cursor_base');
+
+        // this.rect = this.add.rectangle(400, 300, 32, 32); //.setStrokeStyle(2, 0xffff00)
+        // this.cursor = this.add.sprite(0, 0, 'cursor_base');
+        // this.anims.create({
+        //     key: 'cursor_animation',
+        //     frames: this.anims.generateFrameNumbers('cursor', {start: 0, end: 3, first: 0}),
+        //     frameRate: 15,
+        //     repeat: -1,
+        // })
+        // this.cursor.anims.play('cursor_animation');
         KEY_P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
-        // document.body.style.cursor = 'none';
         this.showCursor = true;
         
         
@@ -69,104 +67,10 @@ class menu extends Phaser.Scene {
         }
         this.add.text(game.config.width/2, game.config.height/15, 'ONCE UPON A TIME IN THE WEST: THE GAME', menuConfig).setOrigin(0.5);
         
-        //create revolver UI
-        this.shot_count = 7;
-        this.can_fire = true;
-        this.can_reload = true;
-        this.current_rotation = 0;
-        this.final_rotation = 0;
-        this.rotation_multiplier = 1;
-        this.rotation_lerp = 0;
-        // this.anims.create({
-        //     key: 'revolver_ui',
-        //     frames: this.revolver_ui_frames,
-        //     frameRate: 2,
-        //     repeat: -1,
-        // })
-        this.revolver_ui = this.add.sprite(32, game.config.height-32, 'revolver_ui_base_image');
-        // this.revolver_ui.anims.play('revolver_ui');
+        
+        this.UI.create();
     }
     update() {
-
-
-        if (Phaser.Input.Keyboard.JustDown(KEY_P)) { //turn cursor on or off (visually)
-            if(this.showCursor == true) {
-                this.showCursor = false;
-            } else {
-                this.showCursor = true;
-            }
-        }
-        if(this.showCursor == true) { //turn cursor on or off (visually)
-            document.body.style.cursor = '';
-        } else {
-            document.body.style.cursor = 'none';
-        }
-        // console.log(this.showCursor);
-
-        MOUSE_POINTER = this.input.activePointer;
-
-        // console.log(MOUSE_POINTER.x, MOUSE_POINTER.y); //the mouse location
-        // console.log(MOUSE_POINTER.isDown);//check if any mouse button is down
-        // console.log( MOUSE_POINTER.leftButtonDown(), MOUSE_POINTER.rightButtonDown()); //check if left or right mouse button is down.
-        
-        // this.revolver_ui.rotation += 0.01;
-        if(this.rotation_lerp < 1.0) {
-            this.revolver_ui.angle = Phaser.Math.Linear(this.current_rotation, this.final_rotation, this.rotation_lerp);
-            this.rotation_lerp += 0.1;
-        }
-        // console.log(this.revolver_ui.rotation);
-        console.log(this.shot_count);
-        this.revolver_ui.setTexture(`revolver_ui${this.shot_count}`);
-        
-        if (MOUSE_POINTER.leftButtonDown() && this.can_fire == true) {   //left key down
-            this.cursor.setScale(0.75);
-            if (this.shot_count > 1) {
-                this.shot_count -= 1;
-                this.final_rotation += 60;
-                this.rotation_lerp = 0;
-            }
-            this.can_fire = false;
-        } 
-        
-        if (!MOUSE_POINTER.leftButtonDown()) {                                //left key up
-            this.cursor.setScale(1.0);
-            this.can_fire = true;
-        }
-
-        if (MOUSE_POINTER.rightButtonDown() && this.can_reload == true) {
-            if (this.shot_count < 7) {
-                this.shot_count += 1;
-                this.final_rotation -= 60;
-                this.rotation_lerp = 0;
-            }
-            this.can_reload = false;
-        } 
-        if (!MOUSE_POINTER.rightButtonDown()) {
-            this.can_reload = true;
-        }
-
-
-        // if (this.can_fire == true) {
-        //     if (this.shot_count < 7) {
-        //         this.shot_count += 1;
-        //     }
-        //     this.can_fire = false;
-        // }
-
-        // this.input.on('pointerdown', pointer => {
-        //     console.log("HERE");
-        // }, this);
-
-        // this.input.on('pointermove', (MOUSE_POINTER) =>
-        // {
-        //     this.rect.copyPosition(MOUSE_POINTER);
-        // });
-        this.cursor.x = MOUSE_POINTER.x;
-        this.cursor.y = MOUSE_POINTER.y;
-        
-        this.rect.copyPosition(MOUSE_POINTER)
-        const { left, top, width, height } = this.rect.getBounds();
-        const bodiesInRect = this.physics.overlapRect(left, top, width, height);
-        // console.log(bodiesInRect); //check what's in the rect.
+        this.UI.update();
     }
 }
