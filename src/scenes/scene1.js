5a925c86ba41ac139216db831999749d464a18a8
gejpark <122942803+gejpark@users.noreply.github.com>
2023-06-05 23:22:57 -0700

    asset creation

--- src/scenes/scene1.js
+++ src/scenes/scene1.js
@@ -3,7 +3,7 @@ class scene1 extends Phaser.Scene {
         super('scene1');
     }
     preload() {
-
+        this.load.image('scene1_background', './assets/scene1_images/gameplay/scene1_background.png');
     }
     create() {
         // menu text configuration
@@ -20,8 +20,13 @@ class scene1 extends Phaser.Scene {
             fixedWidth: 0
         }
         this.add.text(game.config.width/2, game.config.height/15, 'SCENE 1', menuConfig).setOrigin(0.5);
+        this.scene1_background = this.add.sprite(0, 0, 'scene1_background').setOrigin(0);
+
+        //create UI
+        this.UI = new UI(this,300,300, 'cursor_base');
+        this.UI.create();
     }
     update() {
-        
+        this.UI.update();
     }
 }
